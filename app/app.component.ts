6c8389b912dc764f4006da7ad622a9b94242784b
Chance Magno and Mark Fisher <chance@>
2017-05-02 11:51:21 -0700

    separated template html into its own file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,36 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div class="container">
-    <h1>Recipe Box</h1>
-    <div class="row">
-      <div class="recipe-display-section col-md-6">
-        <ul>
-          <li *ngFor="let currentRecipe of recipes">
-            <h3 (click)="revealDetails(currentRecipe)">{{currentRecipe.title}}</h3>
-          </li>
-          <div *ngIf=selectedRecipe>
-            <h5>{{selectedRecipe.details}}</h5>
-            <button (click)="hideDetails()">Hide</button>
-            <button (click)="turnOnEditMode()">Edit</button>
-          </div>
-        </ul>
-      </div>
-      <div *ngIf=editMode>
-        <div class="form-display-section col-md-6">
-          <form>
-            <label for="title">Title:</label>
-            <input [(ngModel)]="selectedRecipe.title" name="title">
-            <label for="description">Description:</label>
-            <input [(ngModel)]="selectedRecipe.details" name="description">
-            <button (click)="hideDetails()"  type="button">Done</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-  `
+  templateUrl: './app/app.component.html' 
 })
 
 export class AppComponent {
