230e51ef88de58aef4f1160029ebc84b60e6a897
Chance Magno and Mark Fisher <mark@>
2017-05-02 11:12:52 -0700

    add edit functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,16 +5,30 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Recipe Box</h1>
-    <div class="recipe-display-section">
-      <ul>
-        <li *ngFor="let currentRecipe of recipes">
-          <h3 (click)="revealDetails(currentRecipe)">{{currentRecipe.title}}</h3>
-        </li>
-        <div *ngIf=selectedRecipe>
-          <h5>{{selectedRecipe.details}}</h5>
-          <button (click)="hideDetails()">Hide</button>
+    <div class="row">
+      <div class="recipe-display-section col-md-6">
+        <ul>
+          <li *ngFor="let currentRecipe of recipes">
+            <h3 (click)="revealDetails(currentRecipe)">{{currentRecipe.title}}</h3>
+          </li>
+          <div *ngIf=selectedRecipe>
+            <h5>{{selectedRecipe.details}}</h5>
+            <button (click)="hideDetails()">Hide</button>
+            <button (click)="turnOnEditMode()">Edit</button>
+          </div>
+        </ul>
+      </div>
+      <div *ngIf=editMode>
+        <div class="form-display-section col-md-6">
+          <form>
+            <label for="title">Title:</label>
+            <input [(ngModel)]="selectedRecipe.title" name="title">
+            <label for="description">Description:</label>
+            <input [(ngModel)]="selectedRecipe.details" name="description">
+            <button (click)="hideDetails()"  type="button">Done</button>
+          </form>
         </div>
-      </ul>
+      </div>
     </div>
   </div>
   `
@@ -28,9 +42,15 @@ export class AppComponent {
   ];
 
   selectedRecipe = null;
+  editMode = null;
+
+  turnOnEditMode(){
+    this.editMode = true;
+  }
 
   hideDetails() {
     this.selectedRecipe = null;
+    this.editMode = null;
   }
 
   revealDetails(currentRecipe) {
