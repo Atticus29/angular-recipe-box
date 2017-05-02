5dd3369c692581613d76f0785707dff41d2e30a0
Chance Magno and Mark Fisher <chance@>
2017-05-02 13:28:00 -0700

    have Chance experimenting with firebase

---
+++ app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyCEl-eKObCCKC1jNDZgCSRaMXe8RBRLX_Q",
+  authDomain: "recipe-box-d1a02.firebaseapp.com",
+  databaseURL: "https://recipe-box-d1a02.firebaseio.com",
+  storageBucket: "recipe-box-d1a02.appspot.com",
+  messagingSenderId: "846931653018"
+  };
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent,
+    AlbumDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
-export class AppModule{}
+export class AppModule { }
